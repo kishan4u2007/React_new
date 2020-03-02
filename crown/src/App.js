8ed5b9735f68d63bcc4f251d0a26775967b7cea9
Kishan Raikwar <kishan4u2007@gmail.com>
2020-03-02 23:48:32 +0530

    adding redirect functionality

--- crown/src/App.js
+++ crown/src/App.js
@@ -1,45 +1,35 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
-
-const topic = (props) => {
-  console.log(props)
-  return (
-    <div>Topic</div>
-  )
-}
-
-const topicDetails = (props) => {
-  console.log(props)
-  return (
-  <div>TopicDetails {props.match.params.topicid}</div>
-  )
-}
-
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from "./Redux/user/user.action"
 
 class App extends React.Component {
-  constructor(props) {
-      super(props);
-
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
-
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser:user
-      });
-      console.log(user)
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+
+      setCurrentUser(userAuth);
     });
   }
 
@@ -47,24 +37,29 @@ class App extends React.Component {
     this.unsubscribeFromAuth();
   }
 
- 
-
   render() {
-    console.log(this.props)
     return (
-    <div>
-      <Header currentUser = { this.state.currentUser } />
-      <Switch>
-        <Route  exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/topic' component={topic} />
-        <Route exact path='/topic/:topicid' component={topicDetails} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-    )
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' render={() => this.props.currentUser ? (<Redirect to="/" /> ) :  <SignInAndSignUpPage />  }  />
+        </Switch>
+      </div>
+    );
   }
-  
 }
 
-export default App;
+const mapStateToProps = ({ user }) => ({
+  currentUser : user.currentUser
+})
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
